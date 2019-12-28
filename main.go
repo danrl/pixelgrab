f7e3369779053d52687e5dad31ae2cb7322b4ebc
Dan Lüdtke <mail@danrl.com>
2019-12-29 00:58:56 +0100

    updated version

--- main.go
+++ main.go
@@ -2,30 +2,15 @@ package main
 
 import (
 	"image"
-	"os"
 
-	_ "image/png"
+	"image/color"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
-	"golang.org/x/image/colornames"
 )
 
-func loadPicture(path string) (pixel.Picture, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-	img, _, err := image.Decode(file)
-	if err != nil {
-		return nil, err
-	}
-	return , nil
-}
-
 const (
-	width = 1920
+	width  = 1920
 	height = 1080
 )
 
@@ -49,7 +34,7 @@ func run() {
 
 		// code from michi
 		// for each pixel
-		// img.Set(x, y, color)
+		img.Set(10, 30, color.White) // example
 
 		// update window with current pixels
 		pic := pixel.PictureDataFromImage(img)
