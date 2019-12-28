4babc80be51cac951bf3dad3601fdf2006bfa57a
Dan Lüdtke <mail@danrl.com>
2019-12-29 00:56:23 +0100

    updated version

--- main.go
+++ main.go
@@ -21,34 +21,40 @@ func loadPicture(path string) (pixel.Picture, error) {
 	if err != nil {
 		return nil, err
 	}
-	return pixel.PictureDataFromImage(img), nil
+	return , nil
 }
 
-
+const (
+	width = 1920
+	height = 1080
+)
 
 func run() {
-	cfg := pixelgl.WindowConfig{
-		Title:  "Pixel Rocks!",
-		Bounds: pixel.R(0, 0, 1920, 1080),
+	win, err := pixelgl.NewWindow(pixelgl.WindowConfig{
+		Title:  "Pixel Grab",
+		Bounds: pixel.R(0, 0, width, height),
 		VSync:  true,
-	}
-	win, err := pixelgl.NewWindow(cfg)
+	})
 	if err != nil {
 		panic(err)
 	}
 
-	pic, err := loadPicture("hiking.png")
-	if err != nil {
-		panic(err)
-	}
-
-	sprite := pixel.NewSprite(pic, pic.Bounds())
-
-//	win.Clear(colornames.Greenyellow)
-
-	sprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+	// image data structure to set pixels on
+	img := image.NewRGBA(image.Rectangle{
+		image.Point{0, 0},
+		image.Point{width, height},
+	})
 
 	for !win.Closed() {
+
+		// code from michi
+		// for each pixel
+		// img.Set(x, y, color)
+
+		// update window with current pixels
+		pic := pixel.PictureDataFromImage(img)
+		sprite := pixel.NewSprite(pic, pic.Bounds())
+		sprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
 		win.Update()
 	}
 }
