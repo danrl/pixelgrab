4a152b28424f342d73e780f975ed74f972a2a75c
Dan Lüdtke <mail@danrl.com>
2019-12-29 10:36:13 +0100

    who needs synchronization anyway?

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"image"
+	"image/color"
 	"log"
 	"math/rand"
 	"net"
 	"time"
-
-	"image/color"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 )
 
 const (
-	width       = 1920 / 10
-	height      = 1080 / 10
+	width       = 2048 // actual 4096 but doesn't fit my laptop's screen
+	height      = 768
 	numConns    = 4
 	serverWand  = "151.217.111.34:1234"
-	serverBühne = "151.217.176.193:1234"
+	serverBühne = "151.217.176.193:1234" // 4096x768
 )
 
 type myPixel struct {
@@ -43,69 +43,50 @@ func run() {
 		image.Point{width, height},
 	})
 
-	pixelChan := make(chan *myPixel)
+	//pixelChan := make(chan *myPixel, 100)
 	for i := 0; i < numConns; i++ {
 		go func() {
 			for {
 				conn, err := net.Dial("tcp", serverBühne)
 				if err != nil {
 					log.Printf("dial: %v", err)
-					time.Sleep(50 * time.Millisecond)
+					time.Sleep(200 * time.Millisecond) // give me a break :)
 					continue
 				}
-				line := make([]byte, 2048)
 				for {
-					foo := ""
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-					foo = foo + fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))
-
-					_, _ = conn.Write([]byte(foo))
+					_, _ = conn.Write([]byte(fmt.Sprintf("PX %d %d\n", rand.Int31n(width), rand.Int31n(height))))
 					if err != nil {
 						log.Printf("write: %v", err)
 						conn.Close()
 						break
 					}
-					_, err = conn.Read(line)
-					if err != nil {
-						log.Printf("unable to read: %v", err)
-					}
-
-					px := myPixel{}
-					_, err = fmt.Sscanf(string(line),
-						"PX %d %d %02x%02x%02x%02x",
-						&px.x, &px.y,
-						&px.r, &px.g, &px.b, &px.a)
-					if err != nil {
-						log.Printf("unable to parse `%s`: %v", line, err)
-					}
-					pixelChan <- &px
+					go func() {
+						defer conn.Close()
+						scanner := bufio.NewScanner(conn)
+						for scanner.Scan() {
+							line := scanner.Text()
+							px := myPixel{}
+							_, err = fmt.Sscanf(line,
+								"PX %d %d %02x%02x%02x%02x",
+								&px.x, &px.y,
+								&px.r, &px.g, &px.b, &px.a)
+							if err != nil {
+								log.Printf("unable to parse `%s`: %v", line, err)
+							}
+							img.Set(px.x, px.y, color.RGBA{px.r, px.g, px.b, px.a})
+						}
+						if err := scanner.Err(); err != nil {
+							log.Printf("scan error: %v", err)
+							return
+						}
+					}()
 				}
 			}
 		}()
 	}
 
 	for !win.Closed() {
-		for i := 0; i < 10; i++ {
-			px := <-pixelChan
-			img.Set(px.x, px.y, color.RGBA{px.r, px.g, px.b, px.a})
-		}
-
+		time.Sleep(100 * time.Millisecond) // poor man's frame rate XoXo
 		// update window with current pixels
 		pic := pixel.PictureDataFromImage(img)
 		sprite := pixel.NewSprite(pic, pic.Bounds())
